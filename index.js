785b959a7614e84941d4b1f79b60327c35d22675
Pedro Henrique Pereira de Melo <47577857+indiebr@users.noreply.github.com>
2023-05-21 21:04:05 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-/*Curso de Engenharia de Software - UniEVANGÉLICA 
+/* Curso de Engenharia de Software - UniEVANGÉLICA 
 Disciplina de Programação Web 
 Dev: Pedro Henrique Pereira de Melo 
 DATA 17/05/2023 */
 
-const sequelize = require('../../database');
-const Item = require('./Item');
 
-const models = {
-  Item
-};
+index.js
+Import Express
 
-sequelize.sync({ alter: true });
+const express = require('express');
+const PacienteController = require('./app/controllers/PacienteController');
+const PacienteController = new PacienteController();
+const app = express();
 
-module.exports = models;
+
+app.use(express.json());
+
+app.get('/', function(req, res) {
+  res.send('Bem-vindo!');
+});
+
+app.get('/paciente', (req, res) => PacienteController.findAll(req, res));
+app.post('/paciente', (req, res) => PacienteController.create(req, res));
+app.get('/paciente/:id', (req, res) => PacienteController.findById(req, res));
+app.put('/paciente/:id', (req, res) => PacienteController.update(req, res));
+app.delete('/paciente/:id', (req, res) => PacienteController.delete(req, res));
+
+//Começa o webserver
+app.listen(3000, function() {
+  console.log('Servidor rodando na porta 3000');
+});
+
